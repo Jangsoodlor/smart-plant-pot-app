8b00cb274a77a8fc7ce3cd076c78cbf879ec56bd
Rattapoom Pethputong <celica2548@gmail.com>
2025-04-07 20:28:12 +0700

    Experimenting with charts

--- pages/graph.py
+++ pages/graph.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
+import requests
+import plotly.express as px
 
-df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
 
-st.write(df)
-st.write("Hello world")
+@st.cache_data
+def get_dataframe():
+    url = "http://localhost:8080/moisture/v1/sensor/aggregateHour/3"
+    f = requests.get(url)
+    return pd.read_json(f.text)
+
+st.write("# Average Moisture every 3 hours")
+
+df = get_dataframe()
+# st.line_chart(x='readTime', y='soilMoisture', data=df)
+fig = px.line(x=df['readTime'], y=df['soilMoisture'])
+st.plotly_chart(fig)
+
