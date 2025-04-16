27ff43f48feeae7cbefec1b318ca3e2332243b2f
Rattapoom Pethputong <celica2548@gmail.com>
2025-04-16 22:04:10 +0700

    fix test case

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -2,10 +2,6 @@ import datetime
 import unittest
 
 
-def str_to_time_obj(iso: str) -> datetime.datetime:
-    return datetime.datetime.fromisoformat(iso.replace("Z", "+00:00"))
-
-
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
@@ -25,14 +21,17 @@ class BaseTestCase(unittest.TestCase):
         self.assertIn("precipitation", data)
 
     def assert_frequency_diff(self, data: list[dict], hrs: int):
-        freq_diff = str_to_time_obj(data[-1]["readTime"]) - str_to_time_obj(
+        freq_diff = self.str_to_time_obj(data[-1]["readTime"]) - self.str_to_time_obj(
             data[-2]["readTime"]
         )
         print(freq_diff)
         self.assertEqual(freq_diff.seconds, hrs * 60**2)
 
+    def str_to_time_obj(self, iso: str) -> datetime.datetime:
+        return datetime.datetime.fromisoformat(iso.replace("Z", "+00:00"))
+
     def assert_range_diff(self, data: list[dict], days: int):
-        range_diff = str_to_time_obj(data[-1]["readTime"]) - str_to_time_obj(
-            data[0]["readTime"]
-        )
-        self.assertEqual(range_diff.days, days)
+        time1 = self.str_to_time_obj(data[0]["readTime"])
+        time2 = self.str_to_time_obj(data[-1]["readTime"])
+        range_diff_hours = (time2 - time1).total_seconds() / 3600
+        self.assertGreater(range_diff_hours, (days - 1) * 24)
