288b78886e676a65813e7e4db3b4475750847090
Rattapoom Pethputong <celica2548@gmail.com>
2025-04-20 12:30:20 +0700

    refactor TimeSeriesChart

--- frontend/components/time_series_chart.py
+++ frontend/components/time_series_chart.py
@@ -17,32 +17,29 @@ class TimeSeriesChart:
 
     @classmethod
     @st.cache_data
-    def get_fig(cls, x: str, y: str, frequency: int, data_range: int):
+    def fetch_and_get_fig(cls, x: str, y: str, frequency: int, data_range: int):
         if y in DBAttributes.SENSOR_ATTR.value and y in DBAttributes.WEATHER_ATTR.value:
             df1 = APIFetcher.aggregate_data("sensor", frequency, data_range)
             df2 = APIFetcher.aggregate_data("weather", frequency, data_range)
-            fig = cls.__get_multi_line_chart((df1, "Sensor"), (df2, "API"), x=x, y=y)
-        else:
-            fig = cls.__get_single_line_chart(x, y, frequency, data_range)
-        fig.update_layout(
-            xaxis_title=snake_to_title(x),
-            yaxis_title=f"{cls.append_unit(y)}",
-        )
-        return fig
-
-    @classmethod
-    def __get_single_line_chart(
-        cls, x: str, y: str, frequency: int, data_range: int
-    ) -> go.Figure:
+            fig = cls.get_multi_line_chart(
+                (df1, "Sensor"), (df2, "Weather API"), x=x, y=y
+            )
+            return fig
         if y in DBAttributes.SENSOR_ATTR.value:
             df = APIFetcher.aggregate_data("sensor", frequency, data_range)
         else:
             df = APIFetcher.aggregate_data("weather", frequency, data_range)
-        fig = px.line(data_frame=df, x=x, y=y)
+        fig = cls.get_single_line_chart(df, x, y)
         return fig
 
     @classmethod
-    def __get_multi_line_chart(
+    def get_single_line_chart(cls, df: pd.DataFrame, x: str, y: str):
+        fig = px.line(data_frame=df, x=x, y=y)
+        cls.__add_fig_legends(x, y, fig)
+        return fig
+
+    @classmethod
+    def get_multi_line_chart(
         cls, *dataframes: tuple[pd.DataFrame, str], x: str, y: str
     ):
         fig = go.Figure()
@@ -55,4 +52,12 @@ class TimeSeriesChart:
                     name=f"{source}",
                 )
             )
+        cls.__add_fig_legends(x, y, fig)
         return fig
+
+    @classmethod
+    def __add_fig_legends(cls, x, y, fig):
+        return fig.update_layout(
+            xaxis_title=snake_to_title(x),
+            yaxis_title=f"{cls.append_unit(y)}",
+        )
